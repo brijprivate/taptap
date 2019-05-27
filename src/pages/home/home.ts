4b75bcb226fc949ae7eb794871ade09767f492d7
mefy-brij <brij.singh@mefy.care>
2019-05-27 18:50:18 +0530

    ss

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,7 +6,7 @@ import { NFC, Ndef } from '@ionic-native/nfc';
 import { NfctagProvider } from '../../providers/nfctag/nfctag';
 import { Subscription } from 'rxjs/Rx';
 import { SharedserviceProvider } from '../../providers/sharedservice/sharedservice';
-
+declare var anychart;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -26,129 +26,134 @@ export class HomePage {
 
   doughnutChart: any;
   barChart: any;
-  
-  public userId:any;
-  public userName:any;
 
-  public fashion:"0";
-  public general:"0";
-  public sports:"0";
-  public contact:"0";
-  public event:"0";
-  public groceries:"0";
-  public buisness:"0";
-  public favourite:"0";
-  public totalcount:"0";
-  public todaysTap:"0";
-  public tapItems:any;
+  public userId: any;
+  public userName: any;
+
+  public fashion: "0";
+  public general: "0";
+  public sports: "0";
+  public contact: "0";
+  public event: "0";
+  public groceries: "0";
+  public buisness: "0";
+  public favourite: "0";
+  public totalcount: "0";
+  public todaysTap: "0";
+  public tapItems: any;
 
   lineChart: any;
 
   //NFC read related ....
-  readingTag:   boolean   = false;
-  writingTag:   boolean   = false;
-  isWriting:    boolean   = false;
-  ndefMsg:      string    = '';
+  readingTag: boolean = false;
+  writingTag: boolean = false;
+  isWriting: boolean = false;
+  ndefMsg: string = '';
   subscriptions: Array<Subscription> = new Array<Subscription>();
-  public tapData:any;
+  public tapData: any;
   devicecount: any;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
-    public loginsignupProvider:LoginsignupProvider,
+    public loginsignupProvider: LoginsignupProvider,
     public nfc: NFC,
     public ndef: Ndef,
-    public loading:LoadingController,
-    public nfctagpro:NfctagProvider,
+    public loading: LoadingController,
+    public nfctagpro: NfctagProvider,
     public sharedservice: SharedserviceProvider,
     private toast: ToastController,
-    public alert:AlertController,) 
-  {
+    public alert: AlertController, ) {
     let cdate = new Date().toISOString();
     console.log(cdate);
     this.userId = localStorage.getItem("userId");
     console.log(this.userId);
-    if(this.userId){
+    if (this.userId) {
       this.getprofiledata();
       this.getDashboarddata();
       this.getpresentdateCount();
-      this. getAllTapItem();
+      this.getAllTapItem();
       this.getpairedDevice();
     }
 
-     //Read tag ....
-     this.subscriptions.push(this.nfc.addNdefListener()
-     .subscribe(data => {
-       if (this.readingTag) {
-         let tagid= data.tag.id;
-         // let parsedid = this.nfc.bytesToString(tagid);
-         let payload = data.tag.ndefMessage[0].payload;
-         let tagContent = this.nfc.bytesToString(payload).substring(3);
-         this.readingTag = true;
+    //Read tag ....
+    this.subscriptions.push(this.nfc.addNdefListener()
+      .subscribe(data => {
+        if (this.readingTag) {
+          let tagid = data.tag.id;
+          // let parsedid = this.nfc.bytesToString(tagid);
+          let payload = data.tag.ndefMessage[0].payload;
+          let tagContent = this.nfc.bytesToString(payload).substring(3);
+          this.readingTag = true;
 
-         var s = '';
-         tagid.forEach(function(byte) {
-           s += ('0' + (byte & 0xFF).toString(16)).slice(-2)+':';
-         });
-        
-         console.log("tag data", tagContent);
-         console.log("whole data", data.tag);
-         console.log("tag id", s);
-         this.tapData = s.substring(0, s.length - 1);
-         if(this.tapData){
+          var s = '';
+          tagid.forEach(function (byte) {
+            s += ('0' + (byte & 0xFF).toString(16)).slice(-2) + ':';
+          });
 
-         }
-         return s.substring(0, s.length - 1);
-         
-         } 
-       },
-       err => {
-       })
-     );
+          console.log("tag data", tagContent);
+          console.log("whole data", data.tag);
+          console.log("tag id", s);
+          this.tapData = s.substring(0, s.length - 1);
+          if (this.tapData) {
+
+          }
+          return s.substring(0, s.length - 1);
+
+        }
+      },
+        err => {
+        })
+    );
   }
-  ionViewDidEnter()
-  {
-    if(this.todaysTap)
-    console.log("view enter--------------->>>>>>>>>>>");
+  ionViewDidEnter() {
+    if (this.todaysTap)
+      console.log("view enter--------------->>>>>>>>>>>");
     this.userId = localStorage.getItem("userId");
-    if(this.userId){
+    if (this.userId) {
       this.getprofiledata();
       this.getDashboarddata();
       this.getpresentdateCount();
-      this. getAllTapItem();
+      this.getAllTapItem();
     }
-    if(this.totalcount){
+    if (this.totalcount) {
 
-    
-    var _base = this;
-    setTimeout(function () {
-      _base.doughnutChart = new Chart(_base.doughnutCanvas.nativeElement, {
 
-        type: 'doughnut',
-        data: {
-          labels: ["Total", "Total"],
-          datasets: [{
+      var _base = this;
 
-            // label: '# of Votes',
-            data: [_base.todaysTap, _base.totalcount],
-            // data: [15, 25],
-            backgroundColor: [
-              '#a25757',
-              '#93ca79',
-            ],
-          }]
-        },
-        options: {
-          cutoutPercentage: 80,
-          legend: {
+    }
 
-              display: false,
-          }
-      }
-      });
 
-    }, 3000);
-  }
+    anychart.onDocumentReady(function () {
+      var chart = anychart.pie([
+        { x: "Cycling", value: 10 },
+        { x: "Swimming", value: 12 },
+        { x: "Run", value: 18 },
+        { x: "Hiking", value: 11 },
+        { x: "Alpinism", value: 9 }
+      ]);
+
+      chart.innerRadius("25%");
+
+      var label = anychart.standalones.label();
+
+
+      label.text("taptap");
+
+      label.width("100%");
+      label.height("100%");
+      label.adjustFontSize(true);
+      label.fontColor("#60727b");
+      label.hAlign("center");
+      label.vAlign("middle");
+      chart.legend(false);
+
+      // set the label as the center content
+      chart.center().content(label);
+
+      // chart.title("Donut Chart: Label in the center");
+      chart.container("container");
+      chart.draw();
+    });
   }
 
   selectedTab(index) {
@@ -157,7 +162,7 @@ export class HomePage {
   merchant() {
     // this.navCtrl.push('MerchantPage');
   }
-  category(){
+  category() {
     // this.navCtrl.push('CategoryPage');
   }
   next() {
@@ -168,64 +173,37 @@ export class HomePage {
   }
   ionViewDidLoad() {
     var _base = this;
-    setTimeout(function () {
-      _base.doughnutChart = new Chart(_base.doughnutCanvas.nativeElement, {
 
-        type: 'doughnut',
-        data: {
-          labels: ["Total", "Total"],
-          datasets: [{
-
-            // label: '# of Votes',
-            data: [_base.todaysTap, _base.totalcount],
-            backgroundColor: [
-              '#a25757',
-              '#93ca79',
-            ],
-          }]
-        },
-        options: {
-          cutoutPercentage: 80,
-          legend: {
-
-              display: false,
-          }
-      }
-      });
-
-    }, 3000);
   }
 
-   //Get profile data...
-   getprofiledata(){
+  //Get profile data...
+  getprofiledata() {
     let _base = this;
-    this.loginsignupProvider.getProfile(this.userId).then(function(success:any){
+    this.loginsignupProvider.getProfile(this.userId).then(function (success: any) {
       console.log(success);
-      if(success){
+      if (success) {
         _base.userName = success.result.name;
       }
-    },function(err){
+    }, function (err) {
       console.log(err);
     })
   }
 
   //Tap on product....
-  tapItem()
-  {
-    this.readingTag =true;
+  tapItem() {
+    this.readingTag = true;
     this.navCtrl.push('TapmodalPage');
   }
 
-  getDashboarddata()
-  {
+  getDashboarddata() {
     let _base = this;
     let loader = this.loading.create({
-      content:"Please wait..."
+      content: "Please wait..."
     });
     loader.present();
 
-    this.loginsignupProvider.getDashboard(this.userId).then(function(success:any){
-      console.log("dashboard data ---------->>>>>>"+ success);
+    this.loginsignupProvider.getDashboard(this.userId).then(function (success: any) {
+      console.log("dashboard data ---------->>>>>>" + success);
       console.log(success);
 
       _base.fashion = success.result.fashion;
@@ -238,57 +216,57 @@ export class HomePage {
       _base.groceries = success.result.groceries;
       _base.totalcount = success.result.totalTap;
       loader.dismiss();
-    },function(err){
+    }, function (err) {
       console.log(err);
     })
   }
 
   //get present date tap count....
-  getpresentdateCount(){
+  getpresentdateCount() {
     let _base = this;
-    this.loginsignupProvider.getTapPresentDate(this.userId).then(function(success:any){
+    this.loginsignupProvider.getTapPresentDate(this.userId).then(function (success: any) {
       console.log(success.result);
       _base.todaysTap = success.result.length;
 
-    },function(err){
+    }, function (err) {
       console.log(err);
     })
   }
 
   //get all tap items....
-  getAllTapItem(){
-    let _base  = this;
-    this.loginsignupProvider.getTapAll(this.userId).then(function(success:any){
+  getAllTapItem() {
+    let _base = this;
+    this.loginsignupProvider.getTapAll(this.userId).then(function (success: any) {
       console.log("All Tapped data ,..........>>>>>");
       // console.log(success.result.length);
       _base.tapItems = success.result;
       console.log(_base.tapItems);
-    },function(err){
+    }, function (err) {
       console.log(err);
     })
   }
 
   //go to detail page ...
-  gotoDetails(item){
+  gotoDetails(item) {
     console.log(item);
-    this.navCtrl.push('TapdetailsPage',{itemdetails:item});
+    this.navCtrl.push('TapdetailsPage', { itemdetails: item });
   }
 
   //go to edit profile page ...
-  gotoedit(){
+  gotoedit() {
     this.navCtrl.push('EditprofilePage');
   }
 
   //Get paired devices...
- getpairedDevice(){
-  let _base  = this;
-  this.nfctagpro.getpairdevice(this.userId).then(function(success:any){
-    console.log("paired devices--------------?>>>>>>>>>");
-    console.log(success.result.length);
-    // _base.devices = success.result;
-    _base.devicecount = success.result.length;
-  },function(err){
-    console.log(err);
-  })
-}
+  getpairedDevice() {
+    let _base = this;
+    this.nfctagpro.getpairdevice(this.userId).then(function (success: any) {
+      console.log("paired devices--------------?>>>>>>>>>");
+      console.log(success.result.length);
+      // _base.devices = success.result;
+      _base.devicecount = success.result.length;
+    }, function (err) {
+      console.log(err);
+    })
+  }
 }
