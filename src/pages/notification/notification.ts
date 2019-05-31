e5b5fcf45b1fe1bc4bc9cf1c8babe2a8373b7377
mefy-brij <brij.singh@mefy.care>
2019-05-31 09:38:32 +0530

    notification

--- src/pages/notification/notification.ts
+++ src/pages/notification/notification.ts
@@ -15,11 +15,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class NotificationPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NotificationPage');
+    // this.getnotifications();
+
   }
 
+  // getnotifications() {
+  //   let _base = this;
+  //   let loader = this.loading.create({
+  //     content: "Please wait..."
+  //   });
+  //   loader.present();
+  //   _base.store.getNotifications(localStorage.getItem('userId'))
+  //     .then(function (success: any) {
+  //       loader.dismiss();
+  //       _base.notifications = success.result;
+  //     }, function (error) {
+  //       console.log(error);
+  //       loader.dismiss();
+  //     });
+  // }
+
+
+  // viewNotification(notificationId, seen) {
+  //   let _base = this;
+  //   if (seen) {
+  //     return;
+  //   }
+  //   let loader = this.loading.create({
+  //     content: "Please wait..."
+  //   });
+  //   loader.present();
+  //   _base.store.viewNotification(notificationId)
+  //     .then(function (success: any) {
+  //       loader.dismiss();
+  //       _base.getnotifications();
+  //     }, function (error) {
+  //       console.log(error);
+  //       loader.dismiss();
+  //     });
+  // }
 }
