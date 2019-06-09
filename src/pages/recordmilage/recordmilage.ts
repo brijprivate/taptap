be7a71a1bb39d82128c7dfd1f1efc63451a94782
Pushpendu Ghosh <info@gocubetech.com>
2019-06-09 14:56:50 +0530

    changes

--- src/pages/recordmilage/recordmilage.ts
+++ src/pages/recordmilage/recordmilage.ts
@@ -28,288 +28,302 @@ declare var window;
 export class RecordmilagePage {
 
   //NFC read related ....
-  readingTag:   boolean   = false;
-  writingTag:   boolean   = false;
-  isWriting:    boolean   = false;
-  ndefMsg:      string    = '';
+  readingTag: boolean = false;
+  writingTag: boolean = false;
+  isWriting: boolean = false;
+  ndefMsg: string = '';
   subscriptions: Array<Subscription> = new Array<Subscription>();
 
   //Timer related...
   public timeBegan = null
-  public timeStopped:any = null
-  public stoppedDuration:any = 0
+  public timeStopped: any = null
+  public stoppedDuration: any = 0
   public started = null
   public running = false
   public blankTime = "00:00.00"
   public time = "00:00:00"
 
-  public tapData:any;
-  public isnetwork= "Online";
-  deviceVerify:boolean = false;
-  public record:any;
-  public userId:any;
+  public tapData: any;
+  public isnetwork = "Online";
+  deviceVerify: boolean = false;
+  public record: any;
+  public userId: any;
 
-  public locations:any=[];
-  public chkloc:boolean = false;
-  public totaldis=0;
-  public currentPosition:any;
-  public endLocation:any;
-  public checkp:boolean=false;
-  islocation:boolean=false;
+  public locations: any = [];
+  public chkloc: boolean = false;
+  public totaldis = 0;
+  public currentPosition: any;
+  public endLocation: any;
+  public checkp: boolean = false;
+  islocation: boolean = false;
 
-  constructor(public navCtrl: NavController, 
+
+  // to display on screen only
+  sdistance: any;
+  stime: any;
+
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private backgroundGeolocation: BackgroundGeolocation,
-    public loading:LoadingController,
+    public loading: LoadingController,
     public sharedservice: SharedserviceProvider,
     private nativeGeocoder: NativeGeocoder,
     public nfc: NFC,
     public ndef: Ndef,
-    public nfctagpro:NfctagProvider,
+    public nfctagpro: NfctagProvider,
     private toast: ToastController,
-    public alert:AlertController,
+    public alert: AlertController,
     private locationAccuracy: LocationAccuracy
     // private diagnostic: Diagnostic
-    ) 
-    {
-      this.userId = localStorage.getItem("userId");
-      this.tapData = navParams.get("tapdata");
+  ) {
+    this.userId = localStorage.getItem("userId");
+    this.tapData = navParams.get("tapdata");
 
-       //Get Network status...
-       this.sharedservice.getNetworkStat().subscribe((value)=>{
-        console.log("network status------------------>>>>>>",value);
-        this.isnetwork = value;
-      });
+    //Get Network status...
+    this.sharedservice.getNetworkStat().subscribe((value) => {
+      console.log("network status------------------>>>>>>", value);
+      this.isnetwork = value;
+    });
     //  this.locations = [];
     //  this.startBackgroundTracking();
-    this.sharedservice.getlocation().subscribe((value)=>{
+    this.sharedservice.getlocation().subscribe((value) => {
       console.log("shared location------------>>>>>>>>>>.");
       console.log(value);
-      if(Object.keys(value).length==0 || value == null){
+      if (Object.keys(value).length == 0 || value == null) {
         return;
-      }else{
+      } else {
         this.locations.push(value);
-          console.log("start location------------->>>>>>>",this.locations[0]);
+        console.log("start location------------->>>>>>>", this.locations[0]);
 
-          this.nativeGeocoder.reverseGeocode(this.locations[0].latitude, this.locations[0].longitude)
-          .then((result: NativeGeocoderReverseResult[]) =>{
-            console.log("reverse geocode ----------------->>>>>>>",result)
+        let loader = this.loading.create({
+          content: "Please wait..."
+        });
+        loader.present();
+
+        this.nativeGeocoder.reverseGeocode(this.locations[0].latitude, this.locations[0].longitude)
+          .then((result: NativeGeocoderReverseResult[]) => {
+            console.log("reverse geocode ----------------->>>>>>>", result)
             console.log(JSON.stringify(result[0]));
             this.currentPosition = result[0];
+            this.checkp = true;
+            loader.dismiss();
           });
-      
+
         console.log("array of locations------------->>>>>>>>");
         console.log(this.locations);
       }
-    });     
-    }
+    });
+  }
 
-    ionViewDidLoad(){
+  ionViewDidLoad() {
+    this.enableLocation();
+
+
+    // to display on screen only
+    let _base = this;
+    setInterval(function () {
+      _base.sdistance = _base.totaldis;
+      _base.stime = _base.time;
+    }, 500);
+
+  }
+
+
+  startBackgroundTrack() {
+    this.backgroundGeolocation.start();
+
+    let _base = this;
+    if (this.isnetwork == "Offline") {
+      let showtoast = this.toast.create({
+        message: "Please check your internet connection and try again",
+        duration: 60000,
+        position: "bottom",
+        showCloseButton: true,
+        closeButtonText: "Ok"
+      })
+      showtoast.present();
+      return;
+    }
+    else if (this.islocation == false) {
+
       this.enableLocation();
+      return;
     }
 
+    else if (!this.record) {
+      let showtoast = this.toast.create({
+        message: "Please select one record type before start",
+        duration: 60000,
+        position: "bottom",
+        showCloseButton: true,
+        closeButtonText: "Ok"
+      })
+      showtoast.present();
+      return;
+    }
 
-    startBackgroundTrack()
-    {
-      this.backgroundGeolocation.start();
-      
-      let _base= this;
-      if(this.isnetwork == "Offline")
-      {
-        let showtoast = this.toast.create({
-          message: "Please check your internet connection and try again",
-          duration: 60000,
-          position: "bottom",
-          showCloseButton: true,
-          closeButtonText: "Ok"
-        })
-        showtoast.present();
-        return;
-      }
-      else if(this.islocation == false)
-      {
-        
-        this.enableLocation();
-        return;
-      }
-  
-      else if(!this.record)
-      {
-        let showtoast = this.toast.create({
-          message: "Please select one record type before start",
-          duration: 60000,
-          position: "bottom",
-          showCloseButton: true,
-          closeButtonText: "Ok"
-        })
-        showtoast.present();
-        return;
-      }
-      
-      else if(this.running) return;
+    else if (this.running) return;
 
-      else if (this.timeBegan === null) {
-        this.reset();
-        this.timeBegan = new Date();
+    else if (this.timeBegan === null) {
+      this.reset();
+      this.timeBegan = new Date();
     }
     else if (this.timeStopped != null) {
-      let newStoppedDuration:any = (+new Date() - this.timeStopped)
+      let newStoppedDuration: any = (+new Date() - this.timeStopped)
       this.stoppedDuration = this.stoppedDuration + newStoppedDuration;
     }
 
-    this.started = setInterval(this.clockRunning.bind(this), 10);
+    this.started = setInterval(this.clockRunning.bind(this), 100);
     this.running = true;
     this.loop();
+  }
+  reset() {
+    this.running = false;
+    clearInterval(this.started);
+    this.stoppedDuration = 0;
+    this.timeBegan = null;
+    this.timeStopped = null;
+    this.time = this.blankTime;
+  }
+
+  zeroPrefix(num, digit) {
+    let zero = '';
+    for (let i = 0; i < digit; i++) {
+      zero += '0';
     }
-    reset() {
-      this.running = false;
-      clearInterval(this.started);
-      this.stoppedDuration = 0;
-      this.timeBegan = null;
-      this.timeStopped = null;
-      this.time = this.blankTime;
-    }
-  
-    zeroPrefix(num, digit) {
-      let zero = '';
-      for(let i = 0; i < digit; i++) {
-        zero += '0';
-      }
-      return (zero + num).slice(-digit);
-    }
-  
-    clockRunning()
-    {
-      let currentTime:any = new Date()
-      let timeElapsed:any = new Date(currentTime - this.timeBegan - this.stoppedDuration)
-      let hour = timeElapsed.getUTCHours()
-      let min = timeElapsed.getUTCMinutes()
-      let sec = timeElapsed.getUTCSeconds()
-      let ms = timeElapsed.getUTCMilliseconds();
+    return (zero + num).slice(-digit);
+  }
+
+  clockRunning() {
+    let currentTime: any = new Date()
+    let timeElapsed: any = new Date(currentTime - this.timeBegan - this.stoppedDuration)
+    let hour = timeElapsed.getUTCHours()
+    let min = timeElapsed.getUTCMinutes()
+    let sec = timeElapsed.getUTCSeconds()
+    let ms = timeElapsed.getUTCMilliseconds();
     this.time =
       this.zeroPrefix(hour, 2) + ":" +
       this.zeroPrefix(min, 2) + ":" +
       this.zeroPrefix(sec, 2) + "." +
       this.zeroPrefix(ms, 2);
-    }
-  
+  }
+
 
   //stop tracking .....
-  stopBackgroundTrack(){
+  stopBackgroundTrack() {
     this.navCtrl.push('SavemilagePage',
-    {
-      endtime:this.time,
-      nfcid:this.tapData,
-      recordtype:this.record,
-      distance:this.totaldis,
-      startlocation:this.currentPosition,
-      endLocation:this.endLocation,
-      cords:this.locations
-    });
+      {
+        endtime: this.time,
+        nfcid: this.tapData,
+        recordtype: this.record,
+        distance: this.totaldis,
+        startlocation: this.currentPosition,
+        endLocation: this.endLocation,
+        cords: this.locations
+      });
 
   }
 
 
 
 
-//calculate distance locations loop....
-public loop(){
-  let i=0;
+  //calculate distance locations loop....
+  public loop() {
+    let i = 0;
 
-  var laa1=0
-  var laa2=0
-  var loa1=0
-  var loa2=0
- 
-  for(i=0;i<this.locations.length;i++){
-    console.log("first location------->>>>",i[0]);
-    if(i==this.locations.length-1){
-      return;
-    }
-    laa1=this.locations[i].latitude;
-    laa2=this.locations[i+1].latitude;
-    loa1=this.locations[i].longitude;
-    loa2=this.locations[i+1].longitude;
-    
-    var x=this.distance(laa1,loa1,laa2,loa2,'K');
-    
-    this.totaldis=parseInt( (this.totaldis+x).toFixed(2));
-    
-    console.log('total distance',this.totaldis);
-    // if(this.totaldis){
+    var laa1 = 0
+    var laa2 = 0
+    var loa1 = 0
+    var loa2 = 0
+
+    for (i = 0; i < this.locations.length; i++) {
+      console.log("first location------->>>>", i[0]);
+      if (i == this.locations.length - 1) {
+        return;
+      }
+      laa1 = this.locations[i].latitude;
+      laa2 = this.locations[i + 1].latitude;
+      loa1 = this.locations[i].longitude;
+      loa2 = this.locations[i + 1].longitude;
+
+      var x = this.distance(laa1, loa1, laa2, loa2, 'K');
+
+      this.totaldis = parseInt((this.totaldis + x).toFixed(2));
+
+      console.log('total distance', this.totaldis);
+      // if(this.totaldis){
       console.log("in loooooopppppppppp----------->>>>>>>");
-     
-    // }
-    
+
+      // }
+
+    }
+
+
   }
- 
+  //calculation function....
+  distance(lat1, lon1, lat2, lon2, unit) {
 
-}
-//calculation function....
- distance(lat1, lon1, lat2, lon2, unit) {
-
-	if ((lat1 == lat2) && (lon1 == lon2)) {
-		return 0;
-	}
-	else {
-		var radlat1 = Math.PI * lat1/180;
-		var radlat2 = Math.PI * lat2/180;
-		var theta = lon1-lon2;
-		var radtheta = Math.PI * theta/180;
-		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-		if (dist > 1) {
-			dist = 1;
-		}
-		dist = Math.acos(dist);
-		dist = dist * 180/Math.PI;
-		dist = dist * 60 * 1.1515;
-		if (unit=="K") { dist = dist * 1.609344 }
-		if (unit=="N") { dist = dist * 0.8684 }
-		return dist;
-	}
-}
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+      return 0;
+    }
+    else {
+      var radlat1 = Math.PI * lat1 / 180;
+      var radlat2 = Math.PI * lat2 / 180;
+      var theta = lon1 - lon2;
+      var radtheta = Math.PI * theta / 180;
+      var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+      if (dist > 1) {
+        dist = 1;
+      }
+      dist = Math.acos(dist);
+      dist = dist * 180 / Math.PI;
+      dist = dist * 60 * 1.1515;
+      if (unit == "K") { dist = dist * 1.609344 }
+      if (unit == "N") { dist = dist * 0.8684 }
+      return dist;
+    }
+  }
 
 
-enableLocation()
-{
-this.locationAccuracy.canRequest().then((canRequest: boolean) => {
-console.log("in condition");
-if(canRequest) {
-// the accuracy option will be ignored by iOS
-this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
-() => this.islocation=true,
+  enableLocation() {
+    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
+      console.log("in condition");
+      if (canRequest) {
+        // the accuracy option will be ignored by iOS
+        this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
+          () => this.islocation = true,
 
-error => alert('Error requesting location permissions'+JSON.stringify(error))
-);
-}
+          error => alert('Error requesting location permissions' + JSON.stringify(error))
+        );
+      }
 
-});
-}
+    });
+  }
 
-//check permission...
-// checkpermission(){
-//   console.log("permission");
-//   this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.LOCATION).then(
-//     result => console.log('Has permission?',result.hasPermission),
-//     err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.LOCATION)
-//   );
-// }
+  //check permission...
+  // checkpermission(){
+  //   console.log("permission");
+  //   this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.LOCATION).then(
+  //     result => console.log('Has permission?',result.hasPermission),
+  //     err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.LOCATION)
+  //   );
+  // }
 
-// getPermission() {
-//   this.diagnostic.getPermissionAuthorizationStatus(this.diagnostic.permission.ACCESS_COARSE_LOCATION).then((status) => {
-//     console.log(`AuthorizationStatus`);
-//     console.log(status);
-//     if (status != this.diagnostic.permissionStatus.GRANTED) {
-//       this.diagnostic.requestRuntimePermission(this.diagnostic.permission.ACCESS_COARSE_LOCATION).then((data) => {
-//         console.log(`getCameraAuthorizationStatus`);
-//         console.log(data);
-//       })
-//     } else {
-//       console.log("We have the permission");
-//     }
-//   }, (statusError) => {
-//     console.log(`statusError`);
-//     console.log(statusError);
-//   });
-// }
+  // getPermission() {
+  //   this.diagnostic.getPermissionAuthorizationStatus(this.diagnostic.permission.ACCESS_COARSE_LOCATION).then((status) => {
+  //     console.log(`AuthorizationStatus`);
+  //     console.log(status);
+  //     if (status != this.diagnostic.permissionStatus.GRANTED) {
+  //       this.diagnostic.requestRuntimePermission(this.diagnostic.permission.ACCESS_COARSE_LOCATION).then((data) => {
+  //         console.log(`getCameraAuthorizationStatus`);
+  //         console.log(data);
+  //       })
+  //     } else {
+  //       console.log("We have the permission");
+  //     }
+  //   }, (statusError) => {
+  //     console.log(`statusError`);
+  //     console.log(statusError);
+  //   });
+  // }
 }
