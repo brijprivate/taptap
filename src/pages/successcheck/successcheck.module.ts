c243067c53d538736c6d4eb73dd86985b8640f06
sahapriyanka <psaha2095@gmail.com>
2019-06-12 13:28:47 +0530

    alert,success ui

---
+++ src/pages/successcheck/successcheck.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { SuccesscheckPage } from './successcheck';
+
+@NgModule({
+  declarations: [
+    SuccesscheckPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(SuccesscheckPage),
+  ],
+})
+export class SuccesscheckPageModule {}
