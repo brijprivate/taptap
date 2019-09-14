f12d31bce2c86358466666b38936b861a947d4d6
brijprivate <brijbcet@gmail.com>
2019-09-14 19:22:39 +0530

    popup

---
+++ src/pages/feedpopup/feedpopup.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { FeedpopupPage } from './feedpopup';
+
+@NgModule({
+  declarations: [
+    FeedpopupPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(FeedpopupPage),
+  ],
+})
+export class FeedpopupPageModule {}
---
+++ src/pages/feedpopup/feedpopup.ts
@@ -0,0 +1,37 @@
+import { Component, Renderer } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController, ModalController } from 'ionic-angular';
+
+/**
+ * Generated class for the FeedpopupPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-feedpopup',
+  templateUrl: 'feedpopup.html',
+})
+export class FeedpopupPage {
+  counter=5;
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalController: ModalController,   public renderer: Renderer, public viewCtrl: ViewController,) {
+    let _base=this;
+    setTimeout(() => {
+      _base.renderer.setElementClass(_base.viewCtrl.pageRef().nativeElement, 'feed-popup', true);
+    }, 10);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FeedpopupPage');
+    let _base=this;
+    var x=setInterval(() => {
+      _base.counter= _base.counter-1;;
+      if(_base.counter==0){
+        clearInterval(x);
+        _base.viewCtrl.dismiss({});
+      }
+    }, 1000);
+  }
+
+}
